929ded1c1bea07f30adf83f6e4dd7ded23af544f
Majed A <majed.atwi@gmail.com>
2020-06-28 01:55:12 -0400

    Added rootReducer, mapStateToProps for home and post to get the data from rootReducer, and setup redux components createStore and provider on index.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 import ItemImage from "../itemImage.png";
+import { connect } from 'react-redux';
 
 class Home extends Component {
 
-    state = {
-        posts: []
-    };
-
-    componentDidMount() {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
-            .then((res) => {
-                this.setState({ posts: res.data.slice(0, 10) });
-            });
-    }
-
     render() {
-
-        const { posts } = this.state;
+        const { posts } = this.props;
         const postList = posts.length ? (
             posts.map(post => {
                 return (
                     <div className="post card" key={post.id}>
-                        <img className="item-image" src={ItemImage} alt="Item Image"/>
+                        <img className="item-image" src={ItemImage} alt="Item" />
                         <div className="card-content">
                             <Link to={"/" + post.id} >
                                 <span className="card-title red-text">{post.title}</span>
@@ -47,4 +35,10 @@ class Home extends Component {
     }
 }
 
-export default Home;
+const mapStateToProps = (state) => {
+    return {
+        posts: state.posts
+    }
+}
+
+export default connect(mapStateToProps)(Home);
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
 
 class Post extends Component {
 
-    state = {
-        post: null
-    };
-
-    componentDidMount() {
-        let id = this.props.match.params.post_id;
-
-        axios.get("https://jsonplaceholder.typicode.com/posts/" + id)
-            .then((res) => {
-                this.setState({
-                    post: res.data
-                });
-            });
-    }
-
     render() {
 
-        const post = this.state.post ? (
+        const post = this.props.post ? (
             <div className="post">
-                <h4 className="center">{this.state.post.title}</h4>
-                <p>{this.state.post.body}</p>
+                <h4 className="center">{this.props.post.title}</h4>
+                <p>{this.props.post.body}</p>
             </div>
         ) : (
                 <div className="center">
@@ -38,4 +23,12 @@ class Post extends Component {
     }
 }
 
-export default Post;
+const mapStateToProps = (state, ownProps) => {
+    let id = ownProps.match.params.post_id;
+    console.log("ownProps.match.params: " + ownProps.match.params);
+    return {
+        post: state.posts.find(post => post.id == id)
+    }
+}
+
+export default connect(mapStateToProps)(Post);
